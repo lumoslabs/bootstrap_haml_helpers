89fb5f392055ccfed7f246eb068c8240e58ccb57
Seung-Hyo Choi <seunghyo@lumoslabs.com>
2014-06-17 15:40:14 -0700

    Very slowly start adding initializers for bhh.

--- lib/bootstrap_haml_helpers/engine.rb
+++ lib/bootstrap_haml_helpers/engine.rb
@@ -1,4 +1,10 @@
 module BootstrapHamlHelpers
   class Engine < Rails::Engine
+    initializer 'bootstrap_haml_helpers.load_helpers' do
+      Rails.logger.info "hi hi hi hi hih ih ih"
+      # ActiveSupport.on_load(:action_controller) do
+      #   include Lux::ControllerHelpers
+      # end
+    end
   end
 end
