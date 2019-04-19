aa32d2738b3c9c33429f5928f1db3671cca0e45e
Matt Low <11mdlow@gmail.com>
2019-04-19 16:17:58 -0500

    Change `around_filter` to `around_action`

--- lib/bootstrap_haml_helpers/controller_helpers.rb
+++ lib/bootstrap_haml_helpers/controller_helpers.rb
@@ -2,7 +2,7 @@ module BootstrapHamlHelpers
   module ControllerHelpers
 
     def self.included(base)
-      base.around_filter :init_bhh_context
+      base.around_action :init_bhh_context
       # base.prepend_before_filter :init_page_metadata
     end
 
